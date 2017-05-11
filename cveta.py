76e37b4e0b3c6aae80d0f69e24466b1c0d6e0caa
Ernest farias <efa@wehowsky.com>
2017-05-11 09:45:20 +0200

    args more output

--- cveta.py
+++ cveta.py
@@ -1,21 +1,36 @@
-#cveta cve task automation for ubuntu cve tracker
-#run: "python cveta.py" or "python3 cveta.py"
+# run: "python cveta.py" or "python3 cveta.py"
 #
-#requires:
-#python3 --> python --version to check or use python3 if have both
-#urllib for py3 -->pip3 install urllib
-#bautifulsoup for py 3 -> sudo pip3 install --upgrade beautifulsoup4
+# requires:
+# python3 --> python --version to check or use python3 if have both
+# urllib for py3 -->pip3 install urllib
+# beautifulsoup for py 3 -> sudo pip3 install --upgrade beautifulsoup4
 #
+# TO DO :analize pkg: option to enter a pckg name, output name, source, ver, cves. json file: create a json from the table and process it
+# add input args: select distro, verbose, brief, output to file option.
 import time
 import urllib.request, urllib.error, urllib.parse
-#import urlparse
+# import urlparse
 import sys
 import re
 import subprocess
-#import json
+# import json
 from bs4 import BeautifulSoup
 #from collections import OrderedDict
 import apt
+import argparse
+
+
+# args input
+parseArgs1 = argparse.ArgumentParser()
+# parseArgs1.add_argument("help", help="no help")
+parseArgs1.add_argument('-b', action='store_true', default=False, dest='inBrief', help='Shows only a brief')
+parseArgs1.add_argument('-v', action='store_true', default=False, dest='inVerbose', help='Super Verbose')
+args = parseArgs1.parse_args()
+# print(args.help)
+
+
+
+# apt cache to get local packages
 cache = apt.Cache()
 
 #####vars########### to include as command line options
@@ -66,10 +81,11 @@ except KeyError:
 s = msg
 s2 = msg2
 ### GET TABLE
-soup = BeautifulSoup(s,"lxml")
+soup = BeautifulSoup(s,  "html.parser")
 table = soup.find("table")#, attrs={"class":"details"})
 
-soup2 = BeautifulSoup(s2,"lxml")
+# soup2 = BeautifulSoup(s2,"lxml")
+soup2 = BeautifulSoup(s2,  "html.parser")
 table2 = soup2.find("table")
 
 # The first tr contains the field names.
@@ -84,13 +100,13 @@ headings = [th.get_text() for th in table.find("tr").find_all("th")]
 ########	GET COL NUMBER PER EACH COLUMN NAME OF INTEREST####
 #GET HEADER DISTRO COLUMN NUMBER, assume all tables will have same headers, uso la table 1 porque las demas serian iguales, si no crear uno de estos x cada table
 distroCol = 0
-for th in table.find("tr").find_all("th"): #iterate left to rigth first row as contains th 
+for th in table.find("tr").find_all("th"):# iterate left to rigth first row as contains th
 	for valueth in th: #iterate thcol and get value
 		print(valueth) #	deb
 		if linDist == valueth:
-	#if r'Ubuntu 16.04 LTS (Xenial Xerus)' == valueth:	
+# if r'Ubuntu 16.04 LTS (Xenial Xerus)' == valueth:
 			distroColMatch = distroCol
-	#		print distroColMatch #deb
+# 		print distroColMatch #deb
 		distroCol = distroCol+1	#pruebo la prox col
 		
 #GET HEADER CVE# COLUMN NUMBER
@@ -112,10 +128,10 @@ for th in table.find("tr").find_all("th"): #iterate row
 
 
 ##OLD TB REMOVE
-datasets = []
-for row in table.find_all("tr")[1:]:
-    dataset = list(zip(headings, (td.get_text() for td in row.find_all("td"))))
-    datasets.append(dataset)
+# datasets = []
+# for row in table.find_all("tr")[1:]:
+#   dataset = list(zip(headings, (td.get_text() for td in row.find_all("td"))))
+#    datasets.append(dataset)
 
 
 #jsondata = []
@@ -160,7 +176,7 @@ for row2 in table2.find_all("tr")[1:]:
 	datasetA.append(dataA) #append tuple to array, so i create a list
 	
 	
-#print datasetA #debug
+# print datasetA #debug
 
 pkgNotfoundtotal = 0
 pkgFoundtotal = 0
@@ -168,11 +184,12 @@ pkgFoundcvetotal = 0
 dataPkgwithcve = []
 dataPkgnotfound = []
 dataPkgfound = []
+datasetCVEA = []
 def GetCVEWarning( Ipkgname ):#compares pkcsource name on sys vs pkgname on tables
 	global pkgNotfoundtotal
 	global pkgFoundtotal
 	global pkgFoundcvetotal
-	datasetlocA = []
+	global datasetCVEA
 	global dataPkgnotfound
 	pkgFound = 0
 	global dataPkgfound
@@ -180,21 +197,21 @@ def GetCVEWarning( Ipkgname ):#compares pkcsource name on sys vs pkgname on tabl
 		###for row in table.find_all("tr")[1:]: #el 1: omite la 1er linea que contiene headings, iterate rows
 	for tuplaA in datasetA: #el 1 omite la 1er linea que contiene headings, iterate rows
 		statusA = tuplaA[2] #get text from td that belong to distroStatus
-		cvenoA = tuplaA[1] #cells[cveColMatch].get_text() #get text from td that belong to CVEno
-		pkNameA = tuplaA[0] # cells[pkgnameColMatch].get_text()
+		cvenoA = tuplaA[1]# cells[cveColMatch].get_text() #get text from td that belong to CVEno
+		pkNameA = tuplaA[0]#cells[pkgnameColMatch].get_text()
 		tableNameA = tuplaA[3] #source table universe main etc...
 		if pkNameA == Ipkgname:
 			pkgFound = 1
 			if pkNameA not in dataPkgfound: #count pkgs processed
 				dataPkgfound.append(pkNameA)
 				pkgFoundtotal = pkgFoundtotal + 1
-			if pkNameA == Ipkgname and statusA != 'DNE' and statusA != r'not-affected*' and statusA != r'released*' :
+			if pkNameA == Ipkgname and statusA != 'DNE' and statusA != r'not-affected*' and statusA != r'not-affected' and statusA != r'released*' and statusA != r'released' :
 				if pkNameA not in dataPkgwithcve: #count pkgs with cves
 					dataPkgwithcve.append(pkNameA)
 					pkgFoundcvetotal = pkgFoundcvetotal + 1
 				datalocA = (pkNameA, cvenoA, statusA, tableNameA) #create tuple each time
 		#		print dataA
-				datasetlocA.append(datalocA) #append tuple to array, so i create a list	
+				datasetCVEA.append(datalocA) #append tuple to array, so i create a list
 				print("{0}: {1} :{2} : {3}".format(datalocA[0], datalocA[1], datalocA[2], datalocA[3]))
 	if pkgFound == 0 :
 		if Ipkgname not in dataPkgnotfound: #count pkgs notfound
@@ -206,7 +223,7 @@ def GetCVEWarning( Ipkgname ):#compares pkcsource name on sys vs pkgname on tabl
 
 print("compare with local pkgs")
 
-
+#check by pkcsourne name , DISABLED
 #create scrPkglist unique, contains all pkgs installed on this system- not repeated names
 srcPkglist = set()
 for pkg in cache:
@@ -214,16 +231,36 @@ for pkg in cache:
 #		print(pkg.name)
 #		print(pkg.versions[0].source_name #usar este en vez de name)
 #		time.sleep(3)
+		#srcPkglist.add(pkg.name)
 		srcPkglist.add(pkg.versions[0].source_name)
+#for pkgsource in sorted(srcPkglist): #ordered alpha a-z
+#	GetCVEWarning(pkgsource) #process each pkg
 
-for pkgsource in sorted(srcPkglist): #ordered alpha a-z
-	GetCVEWarning(pkgsource) #process each pkg
+#check by name
+Pkglist = set()
+for pkg in cache:
+	if pkg.is_installed:
+		Pkglist.add(pkg.name)
+	#srcPkglist.add(pkg.versions[0].source_name)
+for pkgname in sorted(Pkglist): #ordered alpha a-z
+	GetCVEWarning(pkgname) #process each pkg
 
 print("\n---REPORT---")
-print(str(pkgFoundcvetotal)  + "\t :Pkgs. with OPEN CVEs on this system") 
-print(str(pkgFoundtotal) + "\t :Pkgs. for this system in Ubuntu tracker")	
+print(str(pkgFoundcvetotal)  + "\t :Pkgs. with OPEN CVEs on this system with " + str(len(datasetCVEA)) + " CVEs" )
+print(str(pkgFoundtotal) + "\t :Pkgs. for this system in Ubuntu CVE tracker")
 print(str(len(dataPkgnotfound)) + "\t :Pkgs with not CVE or not installed from ubuntu repo")
-print(str(len(srcPkglist)) + "\t :Total Pkgs Installed on this system")
+# print(str(len(srcPkglist)) + "\t :Total Pkgs sources installed on this system")
+print(str(len(Pkglist)) + "\t :Total Pkgs Installed on this system, with (" + str(len(srcPkglist)) + ") sources" )
+
+print(
+# '"DNE" means that the package does not exist within the lineage'+
+# '"ignored" means that energy is not being expended for determining whether the problem exists in the particular package within the lineage, because support has ended for one reason or another. See for instance the linux-lts-quantal package, in the Ubuntu 12.04 LTS lineage. Support for that particular package (a backported hardware enablement package) in that lineage is beyond end-of-life.'
+'\n"needs triage" means that the package within the lineage is still supported, but work is needed to determine if the reported problem actually exists.'
+# '"not affected" means that the underlying source code vulnerability exists in the particular package within the lineage, but triage determined that for some other reason the issue will not occur. See for instance "linux-mako" within Ubuntu 16.04 LTS.'
+'\n"needed" of course means that triage has determined that the package within the lineage is affected, but work to apply the fix to the particular package within the lineage is still needed.'
+'\n"pending" means that the work needed to apply the fix to the particular package within the lineage has been done, a version has been cut, and a release is in the works.'
+# '"released" means that the fix for the package within the lineage has been released"'
+)
 
 #1	print datasets2
 
